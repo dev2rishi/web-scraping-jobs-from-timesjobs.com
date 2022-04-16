d9a61a8ceff686b0e2e4aabfc3cb7d28ab7ab025
rishabhSingh <86129510+dev2rishi@users.noreply.github.com>
2022-04-16 14:47:16 +0530

    update main.py

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ def find_jobs():
 if __name__ == '__main__':
     while True:
       find_jobs()
-      time_wait = 1
+      time_wait = 10
       print(f'Waiting {time_wait} minutes')
       time.sleep(time_wait * 60)
